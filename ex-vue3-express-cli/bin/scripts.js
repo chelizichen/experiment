77e59e23b3be25131d32b41cca4549854fe03a80
chelizichen <1347290221@qq.com>
2023-07-05 00:28:34 +0800

    add release

--- ex-vue3-express-cli/bin/scripts.js
+++ ex-vue3-express-cli/bin/scripts.js
@@ -6,7 +6,7 @@ const cwd = require('process').cwd()
 const path = require('path')
 
 function resolve(...args) {
-    return path.resolve(cwd,...args)
+    return path.resolve(cwd, ...args)
 }
 
 const Config = require('webpack-chain')
@@ -14,7 +14,7 @@ const { merge } = require('webpack-merge') // 获取merge函数
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const express = require('express')
-const { spawnSync,spawn,exec } = require("child_process")
+const { spawnSync, spawn, exec } = require("child_process")
 const { mkdirSync } = require("fs")
 
 const app = require(resolve("src", "app.ts")).ServerApplication;
@@ -22,9 +22,9 @@ const app = require(resolve("src", "app.ts")).ServerApplication;
 
 program.version("1.0.0")
     .command("run <args>")
-    .option("-r,--release","is release")
-  .description("up test [dev,build,start]")
-    .action(async function (args,opt) { 
+    .option("-r,--release", "is release")
+    .description("up test [dev,build,start]")
+    .action(async function (args, opt) {
         const commonConfig = require("../config/config")
         const upConfig = require(resolve("up.config.js"))
         // 走dev
@@ -47,23 +47,29 @@ program.version("1.0.0")
             const publicPath = afterMergeConfig.output.publicPath
             const port = devServerOptions.port
 
-            app.use(publicPath, express.static(resolve('public','assets')))
+            app.use(publicPath, express.static(resolve('public', 'assets')))
 
             app.use(
                 webpackDevMiddleware(compiler, {
-                  publicPath, // 公共路径，与 webpack 配置中的 publicPath 保持一致
+                    publicPath, // 公共路径，与 webpack 配置中的 publicPath 保持一致
                 })
             );
-            
+
             app.listen(port, () => {
                 console.log(`webpack out put at  localhost:${port}${publicPath}`);
                 console.log(`Express server is running on localhost:${port}`);
             })
-            
+
         }
         // 走生产打包
         if (args == "prod") {
-            const buildServer = function (){
+            const fse = require('fs-extra');
+            fse.removeSync(resolve("dist"))
+            fse.removeSync(resolve("public", "assets"))
+            fse.removeSync(resolve("release"))
+
+            let publicPath = null;
+            const buildServer = function () {
                 const tsServerJson = resolve('tsconfig.server.json')
                 const serverScripts = ` tsc --project ${tsServerJson} `;
                 const buildProcess = exec(serverScripts);
@@ -76,13 +82,13 @@ program.version("1.0.0")
                 buildProcess.on("exit", function () {
                     console.log("***********服务端打包结束***********");
                     if (opt.release) {
-                        const fse = require('fs-extra');
                         const { appName, serverName } = upConfig;
                         const release_url = resolve('release');
                         const release_src = resolve('release', `${appName}.${serverName}`)
                         const tgz_url = resolve('release', `${appName}.${serverName}.tgz`);
+                        const tgz_name = `${appName}.${serverName}`
                         const pkg = resolve('package.json')
-                        const release_pkg = resolve('release', `${appName}.${serverName}`,'package.json')
+                        const release_pkg = resolve('release', `${appName}.${serverName}`, 'package.json')
                         mkdirSync(release_url)
                         mkdirSync(release_src)
                         fse.copyFileSync(pkg, release_pkg) // 
@@ -90,19 +96,41 @@ program.version("1.0.0")
 
                         const download_production = `cd ${release_src} &&  npm install --production`;
 
-                        const download_cmd = spawn(download_production,{
+                        const download_cmd = spawn(download_production, {
                             stdio: "pipe",
                             shell: true,
                             env: process.env,
                         })
 
-                        
+                        download_cmd.on("exit", function () {
+                            const dist_url = resolve("dist")
+                            const assets_url = resolve("public", "assets")
+                            const target_assets_url = resolve('release', `${appName}.${serverName}`, 'public', 'assets');
+                            fse.copySync(dist_url, release_src + "/dist")
+                            fse.copySync(assets_url, target_assets_url)
+                            console.log("***********打包完成**************")
+
+                            console.log("***********开始压缩**************")
+                            const tgz_cmd = `cd release && tar -zcvf ${tgz_name}.tgz  ./${tgz_name}`;
+                            const cmd_process = exec(tgz_cmd)
+
+                            cmd_process.stderr.on('data', function (chunk) {
+                                console.log('tar', chunk);
+                            })
+                            cmd_process.on('exit',function(){
+                                console.log("***********压缩完成**************")
+                            })
+
+                        })
+
+
+
 
                         // const build_cmd = 'tar .....'
                     }
                 })
             }
-            const buildClient = function (){
+            const buildClient = function () {
                 const { getDevServerOptions } = require("../lib/dev")
                 const buildConfig = getDevServerOptions(upConfig)
                 delete buildConfig.devServer;
@@ -114,9 +142,10 @@ program.version("1.0.0")
                 } catch (error) {
                     throw new Error(`chain call error:${error}`);
                 }
-                const afterMergeConfig = merge(baseConfig, clientConfig.toConfig(),buildConfig)
-                webpack(afterMergeConfig,function (err,stats){
-                    if(err){
+                const afterMergeConfig = merge(baseConfig, clientConfig.toConfig(), buildConfig)
+                publicPath = afterMergeConfig.output.publicPath;
+                webpack(afterMergeConfig, function (err, stats) {
+                    if (err) {
                         console.log(err)
                         return
                     }
@@ -128,7 +157,7 @@ program.version("1.0.0")
             buildClient();
 
             // prod 后 可以走release
-         
+
         }
         // 走运行时
         if (args == "start") {
@@ -136,8 +165,8 @@ program.version("1.0.0")
             const port = upConfig.port
             const publicPath = upConfig.publicPath
             const app = requireEntryFile.ServerApplication
-            app.use(publicPath, express.static(resolve('public','assets')))
-            app.listen(port,function (){
+            app.use(publicPath, express.static(resolve('public', 'assets')))
+            app.listen(port, function () {
                 console.log(`webpack out put at  localhost:${port}${publicPath}`);
                 console.log(`Express server is running on localhost:${port}`);
             })
@@ -147,4 +176,4 @@ program.version("1.0.0")
     })
 
 
- program.parse(process.argv);
+program.parse(process.argv);
